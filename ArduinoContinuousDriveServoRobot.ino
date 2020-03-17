b9ffab07d36e041ce94859e962a712d79d483293
Paul Knauer <paul.knauer@gmail.com>
2020-03-17 21:34:03 +0200

    Tuning the servos

--- ArduinoContinuousDriveServoRobot.ino
+++ ArduinoContinuousDriveServoRobot.ino
@@ -1,26 +1,42 @@
 #include <Servo.h>
 
+#define STOP_POINT 93
+#define STOP_LOWER 83
+#define STOP_UPPER 104
+
 Servo left;
 Servo right;
 
-int i = 0;
+int l = STOP_LOWER;
+int r = STOP_UPPER;
+bool direction = 1;
 
 void setup() {
+  Serial.begin(9600);
   left.attach(9);
   right.attach(10);
 }
 
 void loop() {
   counter();
-  left.write(i);
-  right.write(0);
-  delay(15);
+  left.write(l);
+  right.write(r);
+  Serial.println(l);
+  delay(500);
 }
 
 void counter() {
-  if (i <= 255) {
-    i++;
+  if (direction) {
+    l++;
+    r--;
+    if (l >= STOP_UPPER - 1) {
+      direction = 0;
+    }
   } else {
-    i = 0;
+    l--;
+    r++;
+    if (l <= STOP_LOWER) {
+      direction = 1;
+    }
   }
 }
